bd47433e34937fce32b163d1510025364605e16a
Edward Sriidhar <edwardwaltham@gmail.com>
2020-10-18 15:42:55 -0700

    getters and setters

--- src/Character.java
+++ src/Character.java
@@ -8,10 +8,83 @@ abstract class Character {
 	int dmg;	//physical damage
 	int ap;		//ability power (magic dmg)
 	int arm;	//armor
-	int mr;		//magic resist	
-	int cRes;	//cold resist 
+	int mr;		//magic resist
+	int cRes;	//cold resist
 	int pRes;	//poison resist
 	int fRes;	//fire resist
 	int lRes;	//lightning resist
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getDmg() {
+		return dmg;
+	}
+
+	public void setDmg(int dmg) {
+		this.dmg = dmg;
+	}
+
+	public int getAp() {
+		return ap;
+	}
+
+	public void setAp(int ap) {
+		this.ap = ap;
+	}
+
+	public int getArm() {
+		return arm;
+	}
+
+	public void setArm(int arm) {
+		this.arm = arm;
+	}
+
+	public int getMr() {
+		return mr;
+	}
+
+	public void setMr(int mr) {
+		this.mr = mr;
+	}
+
+	public int getcRes() {
+		return cRes;
+	}
+
+	public void setcRes(int cRes) {
+		this.cRes = cRes;
+	}
+
+	public int getpRes() {
+		return pRes;
+	}
+
+	public void setpRes(int pRes) {
+		this.pRes = pRes;
+	}
+
+	public int getfRes() {
+		return fRes;
+	}
+
+	public void setfRes(int fRes) {
+		this.fRes = fRes;
+	}
+
+	public int getlRes() {
+		return lRes;
+	}
+
+	public void setlRes(int lRes) {
+		this.lRes = lRes;
+	}
+
 	
 }
