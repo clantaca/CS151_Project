f98437b23c93963aac591958e04f8c355248ed8a
Jonstriker <70104642+Jonstriker@users.noreply.github.com>
2020-10-18 16:03:02 -0700

    modified enemy class

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,35 @@
 
-public class Enemy {
-
+public class Enemy extends Character{
+	
+	/*
+	 * power increases by 1 after each enemy is defeated
+	 * when power is at 10, boss is summoned
+	 */
+	int power; 
+	
+	public void createRandomEnemy(int currentPower) {
+		
+	}
+	
+	public void attackUser() {
+		
+	}
+	
+	public void enemyHasBeenDefeated() {
+		
+	}
+	
+	public String toString() {
+		
+		return "";
+				
+				
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("test");
+	}
+	
 }
