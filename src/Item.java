4f14a6a4a8906ff3f4888d971ef90ce30675e4db
Edward Sriidhar <edwardwaltham@gmail.com>
2020-10-18 16:15:53 -0700

    asdf

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,8 @@
-public class Item {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item extends Character {
+    List<Character> items = new ArrayList<Character>(){
+
+    };
+        }
