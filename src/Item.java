3eb7f34dddb263e60e3212925b158020667e1e12
Edward Sriidhar <edwardwaltham@gmail.com>
2020-10-18 16:35:10 -0700

    first item

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,12 @@
-public class Item {
-    public static void main(String[] args) {
-
+public class Item extends Character {
+    public int value;
+    public String itemName;
+    public void Wand (int value) {
+        setFire(fire+2);
+        setCold(cold+2);
+        setLightning(lightning+2);
+        setPoison(poison+2);
+        itemName.equals("Powerful Wand");
     }
+
 }
