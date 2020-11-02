9839bb85a5b56c8288fe3b8727db86adf6400636
Jonstriker <70104642+Jonstriker@users.noreply.github.com>
2020-11-02 15:37:47 -0800

    started gameController.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,4 +1,22 @@
 
 public class GameController {
-
+	
+	private CombatView combatView;
+	private Enemy enemy;
+	private Player player;
+	
+	public GameController(CombatView combatView, Enemy enemy, Player player) {
+		
+		this.combatView = combatView;
+		this.enemy = enemy;
+		this.player = player;
+		
+		//this.combatView.addPhyAtkButListener(new PhyAtkListener());
+		
+	}
+	
+//	class PhyAtkListener implements ActionListner {
+//		
+//	}
+	
 }
