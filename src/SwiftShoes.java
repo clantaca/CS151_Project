d395613aa5d3fa6726d300a853ff7d47918b2452
Edward Sriidhar <edwardwaltham@gmail.com>
2020-11-21 22:04:03 -0800

    New item and better methods

---
+++ src/SwiftShoes.java
@@ -0,0 +1,53 @@
+public class SwiftShoes implements Item {
+
+    private Stats itemStats = new Stats();
+    public int randomInt(int max, int min) {
+        return (int) (Math.random()*(max-min))+min;
+    }
+
+    public SwiftShoes() {
+        itemStats.setHp(randomInt(30, 15));
+        itemStats.setArm(randomInt(10,0));
+        itemStats.setcRes(randomInt(10,0));
+        itemStats.setfRes(randomInt(10,0));
+        itemStats.setpRes(randomInt(10,0));
+        itemStats.setlRes(randomInt(10,0));
+        itemStats.setDodge(25);
+    }
+    @Override
+    public void itemStats() {
+        System.out.println("+" + itemStats.getHp() + " Health");
+        System.out.println("+" + itemStats.getArm() + " Armor");
+        System.out.println("+" + itemStats.getcRes() + " Cold Resistance");
+        System.out.println("+" + itemStats.getfRes() + " Fire Resistance");
+        System.out.println("+" + itemStats.getlRes() + " Lightning Resistance");
+        System.out.println("+" + itemStats.getpRes() + " Poison Resistance");
+
+    }
+
+    @Override
+    public void specialEffect(Player player) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Shoes of Swiftness";
+    }
+
+    @Override
+    public void updatePlayerStats(Player player) {
+        player.getMyStats().setHp(player.getMyStats().getHp() + itemStats.getHp());
+        player.getMyStats().setArm(player.getMyStats().getArm() + itemStats.getArm());
+        player.getMyStats().setcRes(player.getMyStats().getcRes() + itemStats.getcRes());
+        player.getMyStats().setfRes(player.getMyStats().getfRes() + itemStats.getfRes());
+        player.getMyStats().setpRes(player.getMyStats().getpRes() + itemStats.getpRes());
+        player.getMyStats().setlRes(player.getMyStats().getlRes() + itemStats.getlRes());
+        player.getMyStats().setDodge(player.getMyStats().getDodge() + itemStats.getDodge());
+    }
+
+    @Override
+    public String getDescription() {
+        return "Increases Dodge chance by 25%.";
+    }
+}
