17d5107bee8101951f6d8bbf2b9f61c3b98b6dbc
Christine Lantaca <christinedlantaca@gmail.com>
2020-11-25 14:08:43 -0800

    updated notificationView with scrollbar

--- src/NotificationView.java
+++ src/NotificationView.java
@@ -4,71 +4,44 @@ import java.io.PrintStream;
 
 public class NotificationView extends JFrame {
 
-    private int fWidth = 450;
-    private int fHeight = 220;
-    private int border = 10;
-
-    private JPanel nPanel;
-    private JLabel nLabel;
-
-    private JPanel cPanel;
-
     private JTextArea textArea;
-    private PrintStream standardOut;
-
+    private JPanel jpanel;
+    private JLabel jlabel;
+    private JScrollPane scrollPane;
 
     public NotificationView() {
-        createFrame();
-        createNorth();
-        createCenter();
-        this.setVisible(true);
-    }
+        super("Activity Log");
 
-    public void createFrame() {
-        this.setSize(fWidth, fHeight);
-        this.setLayout(new BorderLayout());
-        this.setTitle("Activity Log");
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setVisible(true);
-    }
-    // displays header
-    public void createNorth() {
-        nPanel = new JPanel();
-        nPanel.setLayout(new GridBagLayout());
-        //nPanel.setLayout(new BoxLayout(nPanel, BoxLayout.PAGE_AXIS));
-        nPanel.add(Box.createRigidArea(new Dimension(border, border)));
-        nPanel.setSize(100,100);
-        nPanel.setBackground(Color.YELLOW);
+        // displays activity log title
+        jlabel = new JLabel();
+        jlabel.setText("Activity Log");
+        jpanel = new JPanel();
+        jpanel.setBackground(Color.YELLOW);
+        jpanel.add(jlabel);
 
-        nLabel = new JLabel("Activity Log");
-        nLabel.setHorizontalAlignment(JLabel.CENTER);
-        nPanel.add(nLabel);
-        this.add(nPanel, BorderLayout.NORTH);
-    }
-    // displays activity log information
-    public void createCenter() {
-        cPanel = new JPanel();
-        cPanel.setLayout(new BoxLayout(cPanel, BoxLayout.PAGE_AXIS));
-        cPanel.add(Box.createRigidArea(new Dimension(border, border)));
-        cPanel.setSize(100,100);
-        cPanel.setBackground(Color.LIGHT_GRAY);
-
-        textArea = new JTextArea(50,10);
+        // displays textarea with output stream
+        textArea = new JTextArea(10, 10);
         textArea.setEditable(false);
-        PrintStream printStream = new PrintStream(new ConsoleOutput(textArea));
-        standardOut = System.out;
-        System.setOut(printStream);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        //scrollPane.setBounds(10,60,450,220);
+        System.setOut(new PrintStream(new ConsoleOutput(textArea)));
+        setLayout(new BorderLayout());
+        setBounds(100, 100, 500, 300);
+
+        // adds scrollbar to textarea
+        scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        cPanel.setVisible(true);
-        cPanel.add(textArea);
-        this.add(cPanel, BorderLayout.CENTER);
+        add(jpanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 220);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-
         new NotificationView();
+        System.out.println("This is a test");
+        System.out.println("Hello");
     }
 }
 
