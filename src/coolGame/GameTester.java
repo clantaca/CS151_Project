e028e82e4b8d75cf91b3ee359992d606e2a58e9a
Jonstriker <70104642+Jonstriker@users.noreply.github.com>
2020-11-30 19:31:59 -0800

    created Controller.java

--- src/coolGame/GameTester.java
+++ src/coolGame/GameTester.java
@@ -3,26 +3,32 @@ package coolGame;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import coolGame.controller.MapController;
+import coolGame.controller.Controller;
 import coolGame.controller.Message;
+import coolGame.model.character.Enemy;
 import coolGame.model.character.Player;
+import coolGame.view.CombatView;
 import coolGame.view.MapView;
-import coolGame.view.NotificationView;
 
 public class GameTester {
 
 	private static BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
-	private static MapView view;
+	private static MapView mapView;
+	private static CombatView combatView;
 	private static Player player;
+	private static Enemy enemy;
 	
 	public static void main(String[] args) {
 		player = new Player("Tester");
-		view = MapView.init(player, queue);
+		enemy = new Enemy(1);
+		mapView = MapView.init(player, queue);
+		//combatView = CombatView.init(player, enemy, queue);
 
-		MapController controller = new MapController(view, player, queue);
+		Controller controller = new Controller(mapView, player, queue);
 		
 		controller.mainLoop();
-		view.dispose();
+		mapView.dispose();
+		combatView.dispose();
 		queue.clear();
 	}
 	
